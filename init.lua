516e8e767c1b0be3bac72cadb1a94b661d2dba0b
Garden Gim <gardengim@pm.me>
2024-03-14 13:00:48 +0900

    +plugins, a lot

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ end
 do
 	local set = vim.keymap.set
 	set("n", "<leader>e", "<cmd>Neotree toggle<cr>")
-	set("n", "<leader>f", vim.lsp.buf.format)
+	set("n", "<leader>f", vim.lsp.buf.format, { desc = "LSP Format" })
 end
 
 do
@@ -46,9 +46,7 @@ do
 	require("lazy").setup {
 		{
 			"folke/which-key.nvim",
-			config = function()
-				require('which-key').setup()
-			end
+			config = true
 		},
 		{
 			"rebelot/kanagawa.nvim",
@@ -108,22 +106,62 @@ do
 			'akinsho/bufferline.nvim',
 			version = "*",
 			dependencies = 'nvim-tree/nvim-web-devicons',
-			config = function()
-				require("bufferline").setup {}
-			end
+			config = true
 		},
 		{
 			"petertriho/nvim-scrollbar",
+			config = true
+		},
+		{
+			"folke/noice.nvim",
+			event = "VeryLazy",
+			dependencies = {
+				"MunifTanjim/nui.nvim",
+				"rcarriga/nvim-notify",
+			},
 			config = function()
-				require("scrollbar").setup()
+				require("noice").setup({
+					lsp = {
+						override = {
+							["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+							["vim.lsp.util.stylize_markdown"] = true,
+							["cmp.entry.get_documentation"] = true,
+						},
+					},
+					cmdline = {
+						view = "cmdline"
+					}
+				})
+			end
+		},
+		{
+			"lukas-reineke/indent-blankline.nvim",
+			main = "ibl",
+			opts = {
+				scope = {
+					enabled = true
+				}
+			},
+			config = true
+		},
+		{
+			'nvim-treesitter/nvim-treesitter',
+			build = ":TSUpdate",
+			config = function()
+				local configs = require("nvim-treesitter.configs")
+
+				configs.setup({
+					ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "toml" },
+					sync_install = false,
+					highlight = { enable = true },
+					indent = { enable = true },
+				})
 			end
 		},
 		"neovim/nvim-lspconfig",
 		{
 			"williamboman/mason.nvim",
-			config = function()
-				require("mason").setup()
-			end
+			config = true
 		},
 		{
 			"williamboman/mason-lspconfig.nvim",
@@ -139,20 +177,18 @@ do
 				require("mason-lspconfig").setup_handlers {
 					function(server_name)
 						local lspconfig = require("lspconfig")
-						local config = {}
+
+						local config = {
+							on_attach = on_attach,
+							compatibilities = require('cmp_nvim_lsp').default_capabilities()
+						}
 
 						if server_name == "denols" then
-							config = {
-								on_attach = on_attach,
-								root_dir = lspconfig.util.root_pattern("deno.json",
-									"deno.jsonc"),
-							}
+							config.root_dir = lspconfig.util.root_pattern("deno.json",
+								"deno.jsonc")
 						elseif server_name == "tsserver" then
-							config = {
-								on_attach = on_attach,
-								root_dir = lspconfig.util.root_pattern("package.json"),
-								single_file_support = false
-							}
+							config.root_dir = lspconfig.util.root_pattern("package.json")
+							config.single_file_support = false
 						end
 
 						lspconfig[server_name].setup(config)
@@ -160,5 +196,66 @@ do
 				}
 			end
 		},
+		{
+			"hrsh7th/nvim-cmp",
+			dependencies = {
+				'hrsh7th/cmp-nvim-lsp',
+				'hrsh7th/cmp-buffer',
+				'hrsh7th/cmp-path',
+				'hrsh7th/cmp-cmdline',
+				'hrsh7th/nvim-cmp',
+				'L3MON4D3/LuaSnip',
+				'saadparwaiz1/cmp_luasnip'
+			},
+			config = function()
+				local cmp = require 'cmp'
+
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							require('luasnip').lsp_expand(args.body)
+						end,
+					},
+					completion = { completeopt = 'menu,menuone,noinsert' },
+					mapping = cmp.mapping.preset.insert({
+						['<C-b>'] = cmp.mapping.scroll_docs(-4),
+						['<C-f>'] = cmp.mapping.scroll_docs(4),
+						['<C-Space>'] = cmp.mapping.complete(),
+						['<C-e>'] = cmp.mapping.abort(),
+						['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					}),
+					sources = cmp.config.sources({
+						{ name = 'nvim_lsp' },
+						{ name = 'luasnip' }
+					}, {
+						{ name = 'buffer' },
+					})
+				})
+
+				cmp.setup.filetype('gitcommit', {
+					sources = cmp.config.sources({
+						{ name = 'git' }
+					}, {
+						{ name = 'buffer' },
+					})
+				})
+
+				cmp.setup.cmdline({ '/', '?' }, {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = {
+						{ name = 'buffer' }
+					}
+				})
+
+				cmp.setup.cmdline(':', {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = 'path' }
+					}, {
+						{ name = 'cmdline' }
+					})
+				})
+			end
+		}
 	}
 end
