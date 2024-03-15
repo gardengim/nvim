5f657decf8a009bbfb3bdd9b7e50e8e8249059e3
Garden Gim <gardengim@pm.me>
2024-03-15 16:24:18 +0900

    -simplify

--- init.lua
+++ init.lua
@@ -73,12 +73,8 @@ do
 					local Terminal = require('toggleterm.terminal').Terminal
 					local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
 
-					function _lazygit_toggle()
-						lazygit:toggle()
-					end
-
-					vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>",
-						{ noremap = true, silent = true })
+					vim.keymap.set("n", "<leader>g", function() lazygit:toggle() end,
+						{ desc = "Toggle LaztGit" })
 				end
 			end
 		},
